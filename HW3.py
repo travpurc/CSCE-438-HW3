6bfba951555f92adf4b8152ce57ae3a54f7d034d
Travis Purcell <TravPurc@gmail.com>
2014-03-21 12:47:19 -0500

    Get hit results & formatting Still trying to figure out how to get data organized upon return...

--- HW3.py
+++ HW3.py
@@ -29,7 +29,7 @@ url = "http://www.youtube.com/watch?v=KaqC5FnvAEc"
 #Test URL - Compelex
 #url = "http://www.youtube.com/watch?v=b19l1y7h8XA&list=PLU3TaPgchJtS0U_Qd_bEB5GxAZ4C4_pId"
 #Test URL - Invalid
-url = "http://www.bob.com/watch?v=XXXXXXXXX"
+#url = "http://www.bob.com/watch?v=XXXXXXXXX"
 #Test URL - IS NOT EMBEDDABLE and CAN'T BE KNOWN PRIOR TO ATTEMPTING TO PLAY THE VIDEO
 #TODO: Generate embedded link and make user manually check first...
 #url = "http://www.youtube.com/watch?v=dQw4w9WgXcQ"
@@ -95,7 +95,7 @@ if data_embeddable != "allowed":
 seconds = 0                         #duration of embedded video - last segment is just remaining time of originak video
 embedded_video_length = 20;         #embedded video length is n+1 watch time
 total_time = int(data_duration)     #duration of original video
-count = 0                           #number of video segments  
+count = 0                           #number of video segments (aka HITs)
 video_start = []                    #arrays of the sequential start and end times
 video_end = []
 while (seconds < total_time):       #Build the start and end arrays
@@ -111,11 +111,12 @@ for i in range(0,count):
     embedded_url.append("http://www.youtube.com/embed/"+Video_ID+"?autoplay=1&modestbranding=1&iv_load_policy=3&showinfo=0&rel=0&start="+str(video_start[i])+"&end="+str(video_end[i]))
 
 print embedded_url
+count = len(embedded_url)
 
 #-------------------------------
-#------------ MTURK ------------
+#-------- AWS Connection -------
 #-------------------------------
-'''
+
 ACCESS_ID = raw_input("ACCESS_ID: ")
 SECRET_KEY = raw_input("SECRET_KEY: ");
 HOST = 'mechanicalturk.sandbox.amazonaws.com'
@@ -126,78 +127,89 @@ mtc = MTurkConnection(aws_access_key_id=ACCESS_ID,
 
 print mtc.get_account_balance() 
 
+#-------------------------------
+#-------- HIT Generation -------
+#-------------------------------
 
-title = 'Give your opinion about a website'
-description = ('Visit a website and give us your opinion about'
-               ' the design and also some personal comments')
-keywords = 'website, rating, opinions'
+HIT_IDs = []
+for i in range(0, count):
+    title = 'Give your opinion about a website'
+    description = ('Visit a website and give us your opinion about'
+                   ' the design and also some personal comments')
+    keywords = 'website, rating, opinions'
  
-ratings =[('Very Bad','-2'),
-         ('Bad','-1'),
-         ('Not bad','0'),
-         ('Good','1'),
-         ('Very Good','1')]
+    ratings =[('Very Bad','-2'),
+             ('Bad','-1'),
+             ('Not bad','0'),
+             ('Good','1'),
+             ('Very Good','1')]
  
-#---------------  BUILD OVERVIEW -------------------
+    #---------------  BUILD OVERVIEW -------------------
  
-overview = Overview()
-overview.append_field('Title', 'Give your opinion on this website')
-overview.append(FormattedContent('<a target="_blank"'
-                                 ' href="http://www.toforge.com">'
-                                 ' Mauro Rocco Personal Forge</a>'))
+    overview = Overview()
+    overview.append_field('Title', 'Give your opinion on this website')
+    overview.append(FormattedContent('<a target="_blank"'
+                                     ' href="http://www.toforge.com">'
+                                     ' Mauro Rocco Personal Forge</a>'))
  
-#---------------  BUILD QUESTION 1 -------------------
+    #---------------  BUILD QUESTION 1 -------------------
  
-qc1 = QuestionContent()
-qc1.append_field('Title','How looks the design ?')
+    qc1 = QuestionContent()
+    qc1.append_field('Title','How looks the design ?')
  
-fta1 = SelectionAnswer(min=1, max=1,style='dropdown',
-                      selections=ratings,
-                      type='text',
-                      other=False)
+    fta1 = SelectionAnswer(min=1, max=1,style='dropdown',
+                          selections=ratings,
+                          type='text',
+                          other=False)
  
-q1 = Question(identifier='design',
-              content=qc1,
-              answer_spec=AnswerSpecification(fta1),
-              is_required=True)
+    q1 = Question(identifier='design',
+                  content=qc1,
+                  answer_spec=AnswerSpecification(fta1),
+                  is_required=True)
  
-#---------------  BUILD QUESTION 2 -------------------
+    #---------------  BUILD QUESTION 2 -------------------
  
-qc2 = QuestionContent()
-qc2.append_field('Title','Your personal comments')
+    qc2 = QuestionContent()
+    qc2.append_field('Title','Your personal comments')
  
-fta2 = FreeTextAnswer()
+    fta2 = FreeTextAnswer()
  
-q2 = Question(identifier="comments",
-              content=qc2,
-              answer_spec=AnswerSpecification(fta2))
+    q2 = Question(identifier="comments",
+                  content=qc2,
+                  answer_spec=AnswerSpecification(fta2))
  
-#--------------- BUILD THE QUESTION FORM -------------------
+    #--------------- BUILD THE QUESTION FORM -------------------
  
-question_form = QuestionForm()
-question_form.append(overview)
-question_form.append(q1)
-question_form.append(q2)
+    question_form = QuestionForm()
+    question_form.append(overview)
+    question_form.append(q1)
+    question_form.append(q2)
  
-#--------------- CREATE THE HIT -------------------
+    #--------------- CREATE THE HIT -------------------
  
-new_hit = mtc.create_hit(questions=question_form,
-               max_assignments=1,
-               title=title,
-               description=description,
-               keywords=keywords,
-               duration = 60*5,
-               reward=0.05)
+    new_hit = mtc.create_hit(questions=question_form,
+                   max_assignments=3,
+                   title=title,
+                   description=description,
+                   keywords=keywords,
+                   duration = 60*2,
+                   reward=0.05)
+    
+    
+    print new_hit[0].HITId
+    print new_hit[0].HITTypeId
+    HIT_IDs.append(new_hit[0].HITTypeId)
+    #print "https://workersandbox.mturk.com/mturk/preview?groupId="+new_hit[0].HITTypeId
 
-
-'''
+    i+=1
+    #End of loop
 
 '''
 HIT Data Structure
 
 <HIT>
-  <HITId>123RVWYBAZW00EXAMPLE</HITId>
-  <HITTypeId>T100CN9P324W00EXAMPLE</HITTypeId> - The Website ID
+  <HITId>123RVWYBAZW00EXAMPLE</HITId>          - ID of Hit in HIT group
+  <HITTypeId>T100CN9P324W00EXAMPLE</HITTypeId> - The True HIT ID
   <CreationTime>2005-06-30T23:59:59</CreationTime>
   <HITStatus>Assignable</HITStatus>
   <MaxAssignments>5</MaxAssignments>
@@ -224,12 +236,12 @@ HIT Data Structure
   </QualificationRequirement>
   <HITReviewStatus>NotReviewed</HITReviewStatus>
 </HIT>
-
-print new_hit[0].HITId
-print new_hit[0].HITTypeId
 '''
 
-'''
+#-------------------------------
+#-------- Gather Results -------
+#-------------------------------
+
 def get_all_reviewable_hits(mtc):
     page_size = 50
     hits = mtc.get_reviewable_hits(page_size=page_size)
@@ -249,14 +261,33 @@ def get_all_reviewable_hits(mtc):
         hits.extend(temp_hits)
     return hits
 
-hits = get_all_reviewable_hits(mtc)
- 
-for hit in hits:
-    assignments = mtc.get_assignments(hit.HITId)
-    for assignment in assignments:
-        print "Answers of the worker %s" % assignment.WorkerId
-        for question_form_answer in assignment.answers[0]:
-            for key, value in question_form_answer.fields:
-                print "%s: %s" % (key,value)
-        print "--------------------"
-'''
+#-------------------------------
+#--------- Holding Loop --------
+#-------------------------------
+
+
+while count > 0:
+    hits = []
+    while hits == []:
+        hits = get_all_reviewable_hits(mtc)
+        print hits
+    print "---------------- OUT OF WHILE LOOP -----------------------"
+    for hit in hits:
+        print hit.HITId
+        assignments = mtc.get_assignments(hit.HITId)
+        for assignment in assignments:
+            print "Answers of the worker %s" % assignment.WorkerId
+            for question_form_answer in assignment.answers[0]:
+                try:
+                    for key, value in question_form_answer.fields:
+                        print "%s - %s" % (key,value)
+                except:
+                    for value in question_form_answer.fields:
+                        print value
+            mtc.approve_assignment(assignment.AssignmentId)
+            print "--------------------"
+            mtc.disable_hit(hit.HITId)
+            count-=1 #Got the result from a hit
+    
+print "---------------- OUT OF WHILE LOOP -----------------------"
+
