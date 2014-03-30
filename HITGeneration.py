a3858421ced21c29bad51d2acd379e74851d3bbf
rzav <rzav@users.noreply.github.com>
2014-03-30 11:41:19 -0500

    Updated some styles, have a working save function
    That does not mean we will be using it.  It is just there in case we can use it.  It saves the HIT group ID and all of the identifiers of all HITs created.  It saves the file based on the YouTube title.

--- HITGeneration.py
+++ HITGeneration.py
@@ -9,137 +9,117 @@ Ricardo Zavala
 
 File Created by: Travis Purcell, Ricardo Zavala
 
-Reference:
-HIT Data Structure (returned by create_hit)
-
-<HIT>
-  <HITId>123RVWYBAZW00EXAMPLE</HITId>          - ID of Hit
-  <HITTypeId>T100CN9P324W00EXAMPLE</HITTypeId> - The Website Link ID
-  <CreationTime>2005-06-30T23:59:59</CreationTime>
-  <HITStatus>Assignable</HITStatus>
-  <MaxAssignments>5</MaxAssignments>
-  <AutoApprovalDelayInSeconds>86400</AutoApprovalDelayInSeconds>
-  <LifetimeInSeconds>86400</LifetimeInSeconds>
-  <AssignmentDurationInSeconds>300</AssignmentDurationInSeconds>
-  <Reward>
-    <Amount>25</Amount>
-    <CurrencyCode>USD</CurrencyCode>
-    <FormattedPrice>$0.25</FormattedPrice>
-  </Reward>
-  <Title>Location and Photograph Identification</Title>
-  <Description>Select the image that best represents...</Description>
-  <Keywords>location, photograph, image, identification, opinion</Keywords>
-  <Question>
-    &lt;QuestionForm&gt;
-      [XML-encoded Question data]
-    &lt;/QuestionForm&gt;
-  </Question>
-  <QualificationRequirement>
-    <QualificationTypeId>789RVWYBAZW00EXAMPLE</QualificationTypeId>
-    <Comparator>GreaterThan</Comparator>
-    <Value>18</Value>
-  </QualificationRequirement>
-  <HITReviewStatus>NotReviewed</HITReviewStatus>
-</HIT>
+Purpose:  Contains functions to create HITs
 '''
-
 #-------------------------------
 # ----------- Import -----------
 #-------------------------------
-
-from boto.mturk.question import QuestionContent,Question,HTMLQuestion,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent,FreeTextAnswer
-
+from boto.mturk.question import QuestionContent,Question,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent,FreeTextAnswer
+import os
 #-------------------------------
 #-------- Config Globals -------
 #-------------------------------
 
-#Total Price  = ((Caption_Price * assignmentNum) + Validtion_Price)(video segments) + Tax
+#Total Price  = ((Caption_Price * assignmentNum) + Validation_Price)(video segments) + Tax
 #Choose Wisely...
 Caption_Price = 0.01
-Validtion_Price = 0.01
+Validation_Price = 0.01
 
 #-------------------------------
 # --------- Functions ----------
 #-------------------------------
 
+# Helper function to save the generated HIT and assignments
+def save_file(hit_type_ids, title, n, hit_ids):
+    if hit_type_ids.count(hit_type_ids[0]) == n:
+        print "Creating save file.  File saved will be saved as:"
+        file_name = "%s.sav" % title
+        print file_name
+        if os.path.exists(file_name):
+            print "Collision detected.  Deleting previous save file..."
+            os.remove(file_name)
+        with open(file_name, 'w') as myFile:
+            myFile.write(hit_type_ids[0]+"\n")
+            for r in range(0, len(hit_ids)):
+                myFile.write(hit_ids[r]+"\n")
+        return True
+
+    return False
+
 #Generates N HITs, Returns a list of HIT IDs
-def GenerateCaptionHIT(mtc, n, assignmentNum, embedded_urls):
+def GenerateCaptionHIT(mtc, n, assignmentNum, embedded_urls, data_title):
     HIT_IDs = []
+    HIT_Type_IDs = []
     for i in range(0, n):
         title = 'Provide a caption of a following video'
         description = ('Watch a short video and type in a transcription'
-                       ' of it')
+                        ' of it')
         keywords = 'video, captioning, short'
-        ratings =[('Long (&gt;1 min)','-1'),
-                 ('Medium (&gt;15 secs, &lt;1 min)','0'),
-                 ('Short (&lt;15 secs)','1')]
- 
+        ratings =[('Long (&gt;1 min)','-2'),
+                    ('Medium (&gt;15 secs, &lt;1 min)','-1'),
+                    ('Short (&lt;15 secs)','1')]
+
         #---------------  BUILD OVERVIEW -------------------
- 
         overview = Overview()
         overview.append_field('Title', title)
         overview.append(FormattedContent('<a target="_blank"'
-                                         ' href="' + embedded_urls[i] + '">'
-                                         ' Click here for the short video</a>'
-                                         ))
- 
+                                            ' href="' + embedded_urls[i] + '">'
+                                            ' Click here for the short video</a>'
+                                            ))
+
         #---------------  BUILD QUESTION 1 -------------------
- 
         qc1 = QuestionContent()
         qc1.append_field('Title','Is the video:')
- 
-        fta1 = SelectionAnswer(min=1, max=1,style='dropdown',
-                              selections=ratings,
-                              type='text',
-                              other=False)
+
+        fta1 = SelectionAnswer(min=1, max=1, style='radiobutton',
+                                selections=ratings,
+                                type='text',
+                                other=False)
         #q1 = HTMLQuestion('<iframe id="ytplayer" type="text/html" width="640" height="360" src="'+embedded_urls[i]+'" frameborder="0" allowfullscreen>', 700)
         q1 = Question(identifier='design',
-                      content=qc1,
-                      answer_spec=AnswerSpecification(fta1),
-                      is_required=True)
+                        content=qc1,
+                        answer_spec=AnswerSpecification(fta1),
+                        is_required=True)
  
         #---------------  BUILD QUESTION 2 -------------------
- 
         qc2 = QuestionContent()
         qc2.append_field('Title','What are the words spoken in the video?')
- 
+
         fta2 = FreeTextAnswer()
- 
-        q2 = Question(identifier="comments",
-                      content=qc2,
-                      answer_spec=AnswerSpecification(fta2))
- 
+
+        q2 = Question(identifier='comments',
+                        content=qc2,
+                        answer_spec=AnswerSpecification(fta2))
+
         #--------------- BUILD THE QUESTION FORM -------------------
- 
         question_form = QuestionForm()
         question_form.append(overview)
         question_form.append(q1)
         question_form.append(q2)
  
         #--------------- CREATE THE HIT -------------------
- 
-        #Pretty sure we are going to have to link out to a video...
-        #hitContent = '<!DOCTYPE html> <html lang=en><head><meta charset=utf-8><title>Hello World</title></head><body><h1>VIdeo</h1><p>Just testing....</p></body></html>'
-        #html_question = HTMLQuestion(hitContent, 500)
-
         new_hit = mtc.create_hit(questions=question_form,
-                       max_assignments=assignmentNum,
-                       title=title,
-                       description=description,
-                       keywords=keywords,
-                       duration = 60*2,
-                       reward = Caption_Price)
-    
-    
+                                    max_assignments=assignmentNum,
+                                    title=title,
+                                    description=description,
+                                    keywords=keywords,
+                                    duration=60*2,
+                                    reward=Caption_Price)
         print new_hit[0].HITId
-        #print new_hit[0].HITTypeId
-        print "https://workersandbox.mturk.com/mturk/preview?groupId="+new_hit[0].HITTypeId
-
+        HIT_Type_IDs.append(new_hit[0].HITTypeId)
         HIT_IDs.append(new_hit[0].HITId)
         i+=1
         #End of loop
+
+    #Save the generated HIT so it can be loaded later
+    if not save_file(HIT_Type_IDs, data_title, n, HIT_IDs):
+        print "HITs were created in different groups...\nNo save file can be created."
+
+    print "https://workersandbox.mturk.com/mturk/preview?groupId="+new_hit[0].HITTypeId
+
     return HIT_IDs
 
+###-- NOT EDITED YET --###
 #Generates a validation HIT, Takes a Caption HIT ID, a list of possible answers and returns a validation HIT ID
 def GenerateValidationHIT(mtc, PossibleAnswers, embedded_url):
     title = 'Check the following options for the correct caption'
@@ -148,7 +128,7 @@ def GenerateValidationHIT(mtc, PossibleAnswers, embedded_url):
     keywords = 'video, rating, validation'
  
     ratings = [('No','0'),
-               ('Yes','1')]#,
+               ('Yes','1')]
     CorrectAnswer = [(PossibleAnswers[0],'0'),
                      (PossibleAnswers[1],'1'),
                      ('None of the above','2')]
@@ -202,7 +182,7 @@ def GenerateValidationHIT(mtc, PossibleAnswers, embedded_url):
                     description=description,
                     keywords=keywords,
                     duration = 60 * 2,
-                    reward = Validtion_Price)
+                    reward = Validation_Price)
     
     
     print new_hit[0].HITId
@@ -211,3 +191,37 @@ def GenerateValidationHIT(mtc, PossibleAnswers, embedded_url):
 
     return new_hit[0].HITId
 
+'''
+Reference:
+HIT Data Structure (returned by create_hit)
+
+<HIT>
+  <HITId>123RVWYBAZW00EXAMPLE</HITId>          - ID of Hit
+  <HITTypeId>T100CN9P324W00EXAMPLE</HITTypeId> - The Website Link ID
+  <CreationTime>2005-06-30T23:59:59</CreationTime>
+  <HITStatus>Assignable</HITStatus>
+  <MaxAssignments>5</MaxAssignments>
+  <AutoApprovalDelayInSeconds>86400</AutoApprovalDelayInSeconds>
+  <LifetimeInSeconds>86400</LifetimeInSeconds>
+  <AssignmentDurationInSeconds>300</AssignmentDurationInSeconds>
+  <Reward>
+    <Amount>25</Amount>
+    <CurrencyCode>USD</CurrencyCode>
+    <FormattedPrice>$0.25</FormattedPrice>
+  </Reward>
+  <Title>Location and Photograph Identification</Title>
+  <Description>Select the image that best represents...</Description>
+  <Keywords>location, photograph, image, identification, opinion</Keywords>
+  <Question>
+    &lt;QuestionForm&gt;
+      [XML-encoded Question data]
+    &lt;/QuestionForm&gt;
+  </Question>
+  <QualificationRequirement>
+    <QualificationTypeId>789RVWYBAZW00EXAMPLE</QualificationTypeId>
+    <Comparator>GreaterThan</Comparator>
+    <Value>18</Value>
+  </QualificationRequirement>
+  <HITReviewStatus>NotReviewed</HITReviewStatus>
+</HIT>
+'''
