e722a6121c14684ce9942c24f614763909e924e4
Travis Purcell <TravPurc@gmail.com>
2014-03-31 00:56:36 -0500

    Merging

--- CaptionAndValidate.py
+++ CaptionAndValidate.py
@@ -55,16 +55,22 @@ def find_position(hit_id, completed_hits):
     r = 0
     was_found = False
     can_continue = True
-    while not was_found: 
+    print "Entering Find Loop"
+    while not was_found:
+        print "Inside Find Loop"
         if not can_continue: break
         current_id = completed_hits[r][0]
-        print hit_id
+        print "Current ID:"
         print current_id
-        was_found = hit_id == current_id
+        if hit_id == "":
+            print "hit_id == '' "
+            was_found = True
+            break
         r += 1
         if r == len(completed_hits):
             can_continue = False
     if was_found:
+        print "was found!"
         return r
     print "\n---Shouldn't be here!!---\n"
     return -1
@@ -108,11 +114,26 @@ def validate(answers, completed_hits, mtc, hit_ids, urls):
         r = 0
         s = 1
         id_r = assignment[r][0]
+        print "id_r:"
+        print id_r
         if id_r == "":
             continue
+        print "Length of assignment:"
+        print len(assignment)
         if len(assignment) > 2: # There were repeats... Compare 1 and 3
             s += 1
+        print "s:"
+        print s
         id_s = assignment[s][0]
+        print "id_s:"
+        print id_s
+
+        if len(assignment) > 2: # There were repeats... Compare 1 and 3
+            s -= 1
+        print "s-1:"
+        print s
+        print "new assignment[s][0]:"
+        print assignment[s][0]
         answer1 = assignment[r][1]
         answer2 = assignment[s][1]
         # This should not happen, except after an invalid security answer:
@@ -120,7 +141,10 @@ def validate(answers, completed_hits, mtc, hit_ids, urls):
         u = t
         if id_r != id_s:
             u = find_position(id_s, completed_hits)
+            print u/2
         if similar(answer1, answer2, validationHurdle):
+            print u
+            print u/2
             valid_answers.append([u/2, answer1])
         else:
             make_validation_hit(mtc, [answer1, answer2], urls[hit_ids.index(id_r)])
